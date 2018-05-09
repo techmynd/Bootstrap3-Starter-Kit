0db9e40f4d72631ac533f7efb3b011120be9507f
techmynd <admin@techmynd.com>
2018-05-09 08:53:16 +0500

    form page update

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -62,3 +62,73 @@ $(window).resize(function(){
   // code here
 });
 
+
+/* animate to top */
+$('.top').click(function () {$('body,html').animate({scrollTop: 0}, 800,'easeInOutExpo');
+      return false;
+});
+
+
+/*
+// stick header starts
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+        $(".navbar-fixed-top").addClass("top-opacity");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-opacity");
+    }
+});
+// stick header ends
+*/
+
+/////// Bootstrap Accordian FIX - move control to the top of opened tab - onclick - start
+$('#accordion').on('shown.bs.collapse', function (e) {
+
+  // Validate this panel belongs to this accordian, and not an embedded one
+  var actualAccordianId = $('a[href="#' + $(e.target).attr('id') + '"').data('parent');
+  var targetAccordianId = '#' + $(this).attr('id');
+  if (actualAccordianId !== targetAccordianId) return;
+
+  var clickedHeader = $(this).find('.panel > .collapse.in').closest('.panel').find('.panel-heading');
+  var offset = clickedHeader.offset();
+  var top = $(window).scrollTop();
+  if(offset) {
+    var topOfHeader = offset.top;
+    if(topOfHeader < top) {
+      $('html,body').animate({ scrollTop: topOfHeader}, 100, 'swing');
+    }
+  }
+});
+/////// Bootstrap Accordian FIX - move control to the top of opened tab - onclick - end
+
+/*
+// jquery tabs
+$(function() { $( "#tabs" ).tabs(); });
+// jquery tabs
+// stop jerk on click # based links
+$('#tabs a').click(function(event) {
+event.preventDefault();
+});
+// stop jerk on click # based links ends
+
+// ADD SLIDEDOWN ANIMATION TO DROPDOWN 
+$('.dropdown').on('show.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown("slow");
+});
+
+// ADD SLIDEUP ANIMATION TO DROPDOWN //
+$('.dropdown').on('hide.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp("fast");
+});
+
+// bootstrap accordian - toggle icons - help page
+var selectIds = $('#collapseOne,#collapseTwo,#collapseThree,#collapseFour,#collapseFive,#collapseSix,#collapseSeven,#collapseEight,#collapseNine,#collapseTen');
+$(function ($) {
+    selectIds.on('show.bs.collapse hidden.bs.collapse', function () {
+        $(this).prev().find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+    })
+});
+// bootstrap accordian - toggle icons - help page
+*/
